cd95d99941be393a4085df48ee00a97444b823ee
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2024-12-22 17:12:01 -0800

    make it easier to start simple browsers
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- pkg/browser/browser.go
+++ pkg/browser/browser.go
@@ -2,8 +2,12 @@ package browser
 
 import "golang.org/x/net/html"
 
-// BrowserOpts let you provide custom options to a browser.
+// BrowserOpts let you provide custom options to a browser.  Provide
+// `browser.NO_BROWSER_OPTIONS` when calling Visit() if the browser that you're
+// using does not expose any options.
 type BrowserOpts map[string]interface{}
+
+var NO_BROWSER_OPTIONS = BrowserOpts{}
 
 // Browser is an interface that's used to retrieve
 // online flight data. This makes it easier to retrieve
