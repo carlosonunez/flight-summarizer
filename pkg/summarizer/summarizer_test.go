348b1ae36f8ec56c1fa9b8d62d3f2ed47d777bd7
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2024-12-22 18:10:45 -0800

    add JSON render tests
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

---
+++ pkg/summarizer/summarizer_test.go
@@ -0,0 +1,62 @@
+package summarizer
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSummaryToJSON(t *testing.T) {
+	wantRaw := `
+{
+	"flight_number": "FAKE1",
+	"origin": {
+		"airport": "FOO",
+		"times": {
+				"scheduled": "2024-12-22 17:22 PST",
+				"actual": "2024-12-22 17:00 PST"
+			}
+	},
+	"destination": {
+		"airport": "BAR",
+		"times": {
+			"scheduled": "2024-12-22 18:22 PST",
+			"actual": "2024-12-22 18:00 PST"
+		}
+	}
+}`
+	wantObj := FlightSummary{
+		FlightNumber: "FAKE1",
+		Origin: &Point{
+			AirportIATA: "FOO",
+			Times: &FlightSummaryDateTimes{
+				Scheduled: &Time{mustParseTime("2024-12-22 17:22 PST")},
+				Actual:    &Time{mustParseTime("2024-12-22 17:00 PST")},
+			},
+		},
+		Destination: &Point{
+			AirportIATA: "BAR",
+			Times: &FlightSummaryDateTimes{
+				Scheduled: &Time{mustParseTime("2024-12-22 18:22 PST")},
+				Actual:    &Time{mustParseTime("2024-12-22 18:00 PST")},
+			},
+		},
+	}
+	var want bytes.Buffer
+	require.NoError(t, json.Indent(&want, []byte(wantRaw), "", "	"))
+	got, err := json.MarshalIndent(&wantObj, "", "	")
+	require.NoError(t, err)
+	assert.Equal(t, string(want.Bytes()), string(got))
+}
+
+func mustParseTime(t string) *time.Time {
+	parsed, err := time.Parse("2006-01-02 15:04 MST", t)
+	if err != nil {
+		panic(err)
+	}
+	return &parsed
+}
